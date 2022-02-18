da53a49cf9ebef03359046d99c27693d42b98dbf
Fluke <hunterx52110@gmail.com>
2022-02-18 21:35:06 +0700

    completed

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# NAME:
-# STUDENT ID:
+# NAME:ทัตธน ศรีเงิน
+# STUDENT ID:640612088
--- lab26_2.cpp
+++ lab26_2.cpp
@@ -12,6 +12,20 @@ void printList(list<string> s){
 	cout << "\n";	
 }
 
+#include<iostream>
+#include<list>
+#include<algorithm>
+
+using namespace std;
+
+void printList(list<string> s){
+	list<string>::iterator i;
+	for( i = s.begin(); i != s.end(); i++){
+		cout << *i << " ";
+	}		
+	cout << "\n";	
+}
+
 int main(){
 	
 	list<string> line_up;
@@ -24,6 +38,31 @@ int main(){
 	line_up.insert(loc,"Oscar");
 	
 	//Write your code here
+
+	line_up.push_back("Luffy");
+	line_up.push_back("Sanji");
+	line_up.push_back("Nami");
+
+	
+
+	line_up.pop_front();
+	line_up.pop_front();
+
+
+	loc = find(line_up.begin(), line_up.end(), "Sanji");
+	line_up.insert(loc , "Narutu");
+
+	line_up.push_front("Prayath");
+	
+	loc = find(line_up.begin(), line_up.end(), "Bob");
+	line_up.insert(loc , "Tony");
+
+	loc = find(line_up.begin(), line_up.end(), "Bob");
+	line_up.erase(loc);
+
+	
+
+	for(int i = 0; i<3 ; i++) line_up.pop_front();
 	
 	printList(line_up);
 		
